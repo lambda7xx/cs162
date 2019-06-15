77f51cb2ae7ace809e6ecf63d3af8363c05cd1a7
lambda7xx <lambda7xx@gmail.com>
2019-06-15 14:21:28 +0000

    make the code clean and pass 25 test,only the mlfqs-load-avg and priority-donate-sema  fail

--- pintos/src/threads/synch.c
+++ pintos/src/threads/synch.c
@@ -69,16 +69,11 @@ sema_down (struct semaphore *sema)
   ASSERT (!intr_context ());
 
   old_level = intr_disable ();
-  while (sema->value == 0)//9
-    {  
-      
+  while (sema->value == 0){//9
       //list_push_back (&sema->waiters, &thread_current ()->elem);
-      
        list_insert_ordered(&sema->waiters,&thread_current()->elem,thread_cmp_priority,NULL);//10
-
-      thread_block ();//11
-   //如果sema->vlaue 为0.将其插入sema->waiters,然后调用thread_block(),使thread_current()睡眠。然后完成调度
-   //和线程切换
+       thread_block ();//11
+   /*如果sema->vlaue 为0.将其插入sema->waiters,然后调用thread_block(),使thread_current()睡眠。然后完成调度和线程切换*/
     }
   sema->value--;
   intr_set_level (old_level);
@@ -122,7 +117,7 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters))//1
+  If (!list_empty (&sema->waiters))//1
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));//2
   sema->value++;//3
@@ -208,31 +203,29 @@ lock_acquire (struct lock *lock)
   ASSERT (!lock_held_by_current_thread (lock));
  
  //int priority = thread_current()->priority;
- if(lock->holder != NULL )//线1想要锁0，但锁0在main手里，所以main优先级要变
-	{
+ if(lock->holder != NULL ){//线1想要锁0，但锁0在main手里，所以main优先级要变{
 	struct lock *temp_lock;
 	thread_current()->waiting_threads = lock;
 	temp_lock = lock;
-	while(temp_lock && thread_current()->priority > temp_lock->max_priority )
-	{    
+	while(temp_lock && thread_current()->priority > temp_lock->max_priority){    
 	    temp_lock->max_priority = thread_current()->priority;
             thread_update_priority(temp_lock->holder);	
 	    temp_lock = temp_lock->holder->waiting_threads;
 	}
 }
   sema_down (&lock->semaphore);
- if(lock->holder == NULL)//表示该锁没有被拿到
-  	{
+ if(lock->holder == NULL){//表示该锁没有被拿到{
         lock->max_priority = thread_current()->priority;	
 	thread_hold_lock(lock);
 }
+  //sema_down(&lock->semaphore);
   lock->holder = thread_current();
 }
-void thread_hold_lock(struct lock * lock)
-{
+
+
+void thread_hold_lock(struct lock * lock){
 	enum intr_level old_level = intr_disable();
 	list_insert_ordered(&thread_current()->locks,&lock->elem,lock_cmp_priority, NULL);//插入
-
         int max_priority = list_entry(list_front(&thread_current()->locks),struct lock, elem)->max_priority;
         if(thread_current()->priority < max_priority)//更新优先级
                 thread_current()->priority = max_priority;
