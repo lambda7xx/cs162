6c135e6ba85766f8700ce1b8043b2dd7c2ea16d9
lambda7xx <lambda7xx@gmail.com>
2019-06-28 11:50:03 +0000

    finish the halt system call and now we do the create

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -4,6 +4,7 @@
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "threads/vaddr.h"
+#include "devices/shutdown.h"
 static void syscall_handler (struct intr_frame *);
 
 void
@@ -15,19 +16,40 @@ syscall_init (void)
 static void check_valid_esp(void * vaddr);
 static int write(int fd,const void * buffer, unsigned size);
 static bool right_stack(void * vaddr);
+static void halt(void);/*halt syscall call to terminates pintos */
+
+
+
+
+
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 { check_valid_esp(f->esp);
   uint32_t* args = ((uint32_t*) f->esp);
   //printf("System call number: %d\n", args[0]);
-  if (args[0] == SYS_EXIT) {
+  /*if (args[0] == SYS_EXIT) {
     f->eax = args[1];
     printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
     thread_exit();
   }
  else if(args[0] == SYS_WRITE){
-   f->eax = write(args[1],(void*)args[2],(unsigned )args[3]);
+   f->eax = write(args[1],(void*)args[2],(unsigned )args[3]);*/
+  switch(args[0]){
+	case SYS_HALT:
+		halt();
+		break;
+	case SYS_EXIT:
+		f->eax = args[1];
+		printf("%s: exit(%d)\n", &thread_current ()->name, args[1]);
+    		thread_exit();
+		break;
+	
+	case SYS_WRITE:
+		 f->eax = write(args[1],(void*)args[2],(unsigned )args[3]);
+		 break;
+	
+	
 }
 }
 
@@ -54,4 +76,9 @@ static int  write(int fd, const void *buf, unsigned size)
 }
   
   return result;
+}
+
+static void halt(void){
+	shutdown_power_off();
+
 }
